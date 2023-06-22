677de4915ce665799053ca68ff960fffa824f380
MAC <mac@MacBook-Pro-von-MAC.local>
2023-06-23 00:10:52 +0200

    add template Cards

--- src/features/main/Homepage.tsx
+++ src/features/main/Homepage.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+
 import Header from './Header';
 import Category from './Category';
+import Cards from './Cards';
 
 function Homepage(): JSX.Element {
   return (
     <>
       <Header />
       <Category />
+      <Cards />
       <Outlet />
     </>
   );
