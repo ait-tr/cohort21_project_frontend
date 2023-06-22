2a82de4341c1fd65a9ac0f459e3a5bc13f9604a2
MAC <mac@MacBook-Pro-von-MAC.local>
2023-06-22 23:13:02 +0200

    Category Menu added. No style

--- src/features/main/Homepage.tsx
+++ src/features/main/Homepage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
+import Category from './Category';
 
 function Homepage(): JSX.Element {
   return (
     <>
       <Header />
+      <Category />
       <Outlet />
     </>
   );
