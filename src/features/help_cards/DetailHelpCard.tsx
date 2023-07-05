a1fb38d4d961fd5f399e0ee23c2b5414ea412fe7
MAC <mac@MacBook-Pro-von-MAC.local>
2023-07-05 17:23:26 +0200

    sign or Login

--- src/features/help_cards/DetailHelpCard.tsx
+++ src/features/help_cards/DetailHelpCard.tsx
@@ -9,6 +9,7 @@ import { selectUser } from '../auth/selectors';
 import HelpCard from './types/HelpCard';
 import { getHelpCards } from './helpCardsSlice';
 import Login from '../auth/Login';
+import Register from '../auth/Register';
 
 export default function DetailHelpCard(): JSX.Element {
   const user = useSelector(selectUser);
@@ -22,64 +23,73 @@ export default function DetailHelpCard(): JSX.Element {
   }, [dispatch, id]);
 
   return (
-    <Container>
-      <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
-        {selectedCard && (
-          <Box>
-            <Box sx={{ mb: 1.5, mt: 2.5 }} color="text.secondary">
-              {selectedCard.category.title} / {selectedCard.subCategory.title}
-            </Box>
-            <Box fontSize="2.2rem" component="div" textAlign="center" bgcolor="#e4f5ca" p={2}>
-              {selectedCard.title}
-            </Box>
-
-            <Box textAlign="right" fontSize={48} p={2}>
-              <EuroRoundedIcon />
-              {selectedCard.price}
-            </Box>
-
-            <Box fontSize={24} textAlign="justify" p={2}>
-              {selectedCard.fullDescription}
-            </Box>
-          </Box>
-        )}
-
-        <Divider variant="middle" />
-
-        <Box sx={{ mt: 5, p: 2, bgcolor: '#d8d8d8' }}>
-          <Typography fontSize="1.2rem" gutterBottom variant="h5">
-            Helper info:
-          </Typography>
-          {user ? (
-            selectedCard && (
-              <Box fontSize="1.6rem" display="flex" alignItems="center">
-                <Avatar
-                  variant="square"
-                  alt="Remy Sharp"
-                  src="https://avatars.mds.yandex.net/i?id=7808f22d2c74cc72b53378dc5b5479650088c558-7663734-images-thumbs&n=13"
-                  sx={{ width: 128, height: 128, mr: '0.5rem' }}
-                />
-                <div>
-                  <Typography variant="h6" gutterBottom>
-                    {selectedCard.user.username}
-                  </Typography>
-                  <Typography fontSize="1.6rem" gutterBottom>
-                    Email: {selectedCard.user.email}
-                  </Typography>
-                  <Typography fontSize="1.6rem" gutterBottom>
-                    Phone: {selectedCard.user.phone}
-                  </Typography>
-                </div>
-              </Box>
-            )
-          ) : (
-            <>
-              <Typography>Contact Information Only for authorisated Users</Typography>
-              <Login />
-            </>
-          )}
+<Container>
+  <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+    {selectedCard && (
+      <Box>
+        {/* Card details */}
+        <Box sx={{ mb: 1.5, mt: 2.5 }} color="text.secondary">
+          {selectedCard.category.title} / {selectedCard.subCategory.title}
+        </Box>
+        <Box fontSize="2.2rem" component="div" textAlign="center" bgcolor="#e4f5ca" p={2}>
+          {selectedCard.title}
+        </Box>
+        <Box textAlign="right" fontSize={48} p={2}>
+          <EuroRoundedIcon />
+          {selectedCard.price}
+        </Box>
+        <Box fontSize={24} textAlign="justify" p={2}>
+          {selectedCard.fullDescription}
         </Box>
       </Box>
-    </Container>
+    )}
+
+    <Divider variant="middle" />
+
+    <Box sx={{ mt: 5, p: 2, bgcolor: '#d8d8d8' }}>
+      <Typography fontSize="1.2rem" gutterBottom variant="h5">
+        Helper info:
+      </Typography>
+
+      {user ? (
+        // User is authenticated
+        selectedCard && (
+          <Box fontSize="1.6rem" display="flex" alignItems="center">
+            <Avatar
+              variant="square"
+              alt="Remy Sharp"
+              src="https://avatars.mds.yandex.net/i?id=7808f22d2c74cc72b53378dc5b5479650088c558-7663734-images-thumbs&n=13"
+              sx={{ width: 128, height: 128, mr: '0.5rem' }}
+            />
+            <div>
+              <Typography variant="h6" gutterBottom>
+                {selectedCard.user.username}
+              </Typography>
+              <Typography fontSize="1.6rem" gutterBottom>
+                Email: {selectedCard.user.email}
+              </Typography>
+              <Typography fontSize="1.6rem" gutterBottom>
+                Phone: {selectedCard.user.phone}
+              </Typography>
+            </div>
+          </Box>
+        )
+      ) : (
+        // User is not authenticated
+        <Box sx={{ display: 'flex' }}>
+          <Box sx={{ flex: '1 0 50%', pr: '0.5rem' }}>
+            <Typography variant="h6">Do you have an account?</Typography>
+            <Login />
+          </Box>
+          <Box sx={{ flex: '1 0 50%', pl: '0.5rem' }}>
+            <Typography variant="h6">No account? Register</Typography>
+            <Register />
+          </Box>
+        </Box>
+      )}
+    </Box>
+  </Box>
+</Container>
+
   );
 }
