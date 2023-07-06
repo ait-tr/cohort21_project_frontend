2e7900551868650cfdc58a97f3ae90fccead5b38
AlexDiz <117119877+AlexDiz88@users.noreply.github.com>
2023-07-06 16:48:36 +0200

    Update ProfileInfo.tsx

--- src/features/profile/ProfileInfo.tsx
+++ src/features/profile/ProfileInfo.tsx
@@ -6,15 +6,17 @@ import { selectUser } from '../auth/selectors';
 import { useAppDispatch } from '../../store';
 import { editProfile, getUserCards } from '../auth/authSlice';
 import ProfileEdit from './ProfileEdit';
+import UserImageUploadForm from '../main/UploadForm';
 
 export default function ProfileInfo(): JSX.Element {
   const navigate = useNavigate();
   const user = useSelector(selectUser);
-  const role = user?.role;
+
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const dispatch = useAppDispatch();
   const [isEditingProfile, setIsEditingProfile] = useState(false);
+  const [showUploadForm, setShowUploadForm] = useState(false);
 
   const handleAddCard = (): void => {
     navigate('/card');
@@ -64,16 +66,30 @@ export default function ProfileInfo(): JSX.Element {
             fontWeight={600}
             component="div"
             textAlign="center"
-            bgcolor="#ff9404"
+            bgcolor="#FF9d25"
           >
             Welcome, {user.username}
           </Box>
           <Avatar
             variant="square"
-            alt="Remy Sharp"
-            src="https://avatars.mds.yandex.net/i?id=7808f22d2c74cc72b53378dc5b5479650088c558-7663734-images-thumbs&n=13"
+            alt="avatar"
+            src={`${process.env.PUBLIC_URL}/upload/${user.avatar}`}
             sx={{ width: 128, height: 128, mt: '1rem' }}
           />
+          {!showUploadForm && (
+            <Button
+              sx={{ mt: 1, fontSize: '0.74rem' }}
+              variant="contained"
+              size="small"
+              color="info"
+              onClick={() => setShowUploadForm(true)}
+            >
+              Change image
+            </Button>
+          )}
+          {showUploadForm && (
+            <UserImageUploadForm onClose={() => setShowUploadForm(false)} />
+          )}
           <Box fontWeight={600} mt="0.5rem">
             Email: {user.email}
           </Box>
@@ -94,23 +110,25 @@ export default function ProfileInfo(): JSX.Element {
                   }}
                   onClick={() => setIsEditingProfile(true)}
                 >
-                  Edit Profile
+                  Edit Contacts
                 </Button>
-                {role !== 'ADMIN' ?(
-                <Button
-                  onClick={handleAddCard}
-                  type="button"
-                  variant="contained"
-                  color="info"
-                  fullWidth={false}
-                  sx={{
-                    maxWidth: '12rem',
-                    mr: '1rem', // ? при сжатии переместить
-                  }}
-                >
-                  Add new Card
-                </Button>
-                ):( <Divider variant="middle" />)}
+                {user.role !== 'ADMIN' ? (
+                  <Button
+                    onClick={handleAddCard}
+                    type="button"
+                    variant="contained"
+                    color="info"
+                    fullWidth={false}
+                    sx={{
+                      maxWidth: '12rem',
+                      mr: '1rem', // ? при сжатии переместить
+                    }}
+                  >
+                    Add Help Offer
+                  </Button>
+                ) : (
+                  <Divider variant="middle" />
+                )}
               </>
             )}
 
